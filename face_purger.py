d3e1873c620703be67ecbf8f8fe7821248116081
Ryan Maugin <ryanmaugv1@gmail.com>
2021-03-27 15:04:32 +0000

    Initial commit with PY file.

---
+++ face_purger.py
@@ -0,0 +1,12 @@
+"""
+
+    Face Purger
+
+    Blender plugin for optimising meshes by removing/purging hidden faces
+    using various strategies and allowing control.
+
+    Authored By Ryan Maugin (@ryanmaugv1)
+
+"""
+
+import bpy
