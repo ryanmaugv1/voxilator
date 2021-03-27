8c1ed8bb13102bfcbc0a850cae36c7fc0961b7bc
Ryan Maugin <ryanmaugv1@gmail.com>
2021-03-27 15:05:29 +0000

    Added empty base unselected face filter operator.

--- face_purger.py
+++ face_purger.py
@@ -10,3 +10,23 @@
 """
 
 import bpy
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#   Operators
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+class UnselectedFaceFilterOperator(bpy.types.Operator):
+    """Operator for filtering/removing unselected faces from mesh."""
+
+    bl_idname = 'fpurger.unselected_face_purger'
+    bl_label  = 'Filters/Removes Unselected Faces'
+
+    def execute(self, context):
+        scene = context.scene
+        return {'FINISHED'}
+
+
